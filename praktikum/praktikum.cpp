fef9509350e7721d491c76cb98c2a9d1e8ffe524
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-06 11:06:14 +0700

    class mahasiswa

--- praktikum/praktikum.cpp
+++ praktikum/praktikum.cpp
@@ -1,9 +1,29 @@
 
 #include <iostream>
+using namespace std;
+
+
+class mahasiswa {
+public:
+    int nim;
+    void shownim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    mahasiswa mhs{ 1 };
+    mhs.shownim();
+
+    mahasiswa &refmhs = mhs;
+    refmhs.nim = 2;
+    mhs.shownim();
+
+    mahasiswa *pmhs = &mhs;
+    pmhs->nim = 3;
+    mhs.shownim();
+    return 0;
 }
 
 
